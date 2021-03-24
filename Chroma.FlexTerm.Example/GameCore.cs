9322a9fc5fe0cbb0aebb4cd3a1a7370f6d48813a
vddCore <vddcore@outlook.com>
2021-03-24 20:58:54 +0100

    Add cp437+

--- Chroma.FlexTerm.Example/GameCore.cs
+++ Chroma.FlexTerm.Example/GameCore.cs
@@ -1,5 +1,5 @@
-﻿using System.IO;
-using System.Numerics;
+﻿using System.Numerics;
+using Chroma.Diagnostics;
 using Chroma.Graphics;
 using Chroma.Input;
 
@@ -11,7 +11,7 @@ namespace Chroma.FlexTerm.Example
 
         public GameCore() : base(new(false, false))
         {
-            Window.GoWindowed(new(640,480));
+            Window.GoWindowed(new(1024, 660));
         }
 
         protected override void LoadContent()
@@ -19,49 +19,72 @@ namespace Chroma.FlexTerm.Example
             _terminal = new Terminal(
                 Vector2.Zero,
                 Window.Size,
-                TerminalFont.Copam_8x8
+                TerminalFont.IBM_VGA_8x16
             );
 
-            _terminal.InputReceived += (sender, args) =>
+            _terminal.InputReceived += (_, args) =>
             {
-                if (args.Text == "yeet")
+                if (args.Text == "æ")
                 {
-                    _terminal.Write("I like.\n");
+                    _terminal.Write("<insert random kid having their fingers amputated by car door here>\n");
                 }
             };
 
-            _terminal.Write(
-                "A quick brown fox jumps over the lazy dog.\n" +
-                "0123456789 ¿?¡!`'\"., <>()[]{} &@%*^#$\\/\n" +
-                "\n" +
-                "* Wieniläinen sioux'ta puhuva ökyzombie diggaa Åsan roquefort-tacoja.\n" +
-                "* Ça me fait peur de fêter noël là, sur cette île bizarroïde où une mère et sa môme essaient de me tuer avec un gâteau à la cigüe brûlé.\n" +
-                "* Zwölf Boxkämpfer jagten Eva quer über den Sylter Deich.\n" +
-                "* El pingüino Wenceslao hizo kilómetros bajo exhaustiva lluvia y frío, añoraba a su querido cachorro.\n" +
-                "┌─┬─┐ ╔═╦═╗ ╒═╤═╕ ╓─╥─╖\n" +
-                "│ │ │ ║ ║ ║ │ │ │ ║ ║ ║\n" +
-                "├─┼─┤ ╠═╬═╣ ╞═╪═╡ ╟─╫─╢\n" +
-                "└─┴─┘ ╚═╩═╝ ╘═╧═╛ ╙─╨─╜\n" +
-                "\n" +
-                "░░░░░ ▐▀█▀▌ .·∙•○°○•∙·.\n" +
-                "▒▒▒▒▒ ▐ █ ▌ ☺☻ ♥♦♣♠ ♪♫☼\n" +
-                "▓▓▓▓▓ ▐▀█▀▌  $ ¢ £ ¥ ₧\n" +
-                "█████ ▐▄█▄▌ ◄►▲▼ ←→↑↓↕↨\n" +
-                "\n" +
-                "⌠\n" +
-                "│dx ≡ Σ √x²ⁿ·δx\n" +
-                "⌡\n\n"
-            );
+            var cp437 = "A quick brown fox jumps over the lazy dog.\n" +
+                        "0123456789 ¿?¡!`'\"., <>()[]{} &@%*^#$\\/\n" +
+                        "\n" +
+                        "* Wieniläinen sioux'ta puhuva ökyzombie diggaa Åsan roquefort-tacoja.\n" +
+                        "* Ça me fait peur de fêter noël là, sur cette île bizarroïde où une mère et sa môme essaient de me tuer avec un gâteau à la cigüe brûlé.\n" +
+                        "* Zwölf Boxkämpfer jagten Eva quer über den Sylter Deich.\n" +
+                        "* El pingüino Wenceslao hizo kilómetros bajo exhaustiva lluvia y frío, añoraba a su querido cachorro.\n" +
+                        "┌─┬─┐ ╔═╦═╗ ╒═╤═╕ ╓─╥─╖\n" +
+                        "│ │ │ ║ ║ ║ │ │ │ ║ ║ ║\n" +
+                        "├─┼─┤ ╠═╬═╣ ╞═╪═╡ ╟─╫─╢\n" +
+                        "└─┴─┘ ╚═╩═╝ ╘═╧═╛ ╙─╨─╜\n" +
+                        "\n" +
+                        "░░░░░ ▐▀█▀▌ .·∙•○°○•∙·.\n" +
+                        "▒▒▒▒▒ ▐ █ ▌ ☺☻ ♥♦♣♠ ♪♫☼\n" +
+                        "▓▓▓▓▓ ▐▀█▀▌  $ ¢ £ ¥ ₧\n" +
+                        "█████ ▐▄█▄▌ ◄►▲▼ ←→↑↓↕↨\n" +
+                        "\n" +
+                        "⌠\n" +
+                        "│dx ≡ Σ √x²ⁿ·δx\n" +
+                        "⌡\n\n";
+
+            var plus = "* Quizdeltagerne spiste jordbær med fløde, mens cirkusklovnen Wolther spillede på xylofon.\n" +
+                       "* Ταχίστη αλώπηξ βαφής ψημένη γη, δρασκελίζει υπέρ νωθρού κυνός.\n" +
+                       "* Le cœur déçu mais l'âme plutôt naïve, Louÿs rêva de crapaüter en canoë au delà des îles, près du mälström où brûlent les novæ.\n" +
+                       "* D'fhuascail Íosa, Úrmhac na hÓighe Beannaithe, pór Éava agus Ádhaimh.\n" +
+                       "* Árvíztűrő tükörfúrógép.\n" +
+                       "* Kæmi ný öxi hér ykist þjófum nú bæði víl og ádrepa.\n" +
+                       "* Pchnąć w tę łódź jeża lub ośm skrzyń fig\n" +
+                       "* В чащах юга жил бы цитрус? Да, но фальшивый экземпляр!\n" +
+                       "* Pijamalı hasta, yağız şoföre çabucak güvendi.\n" +
+                       "* »TECHNICIÄNS ÖF SPÅCE SHIP EÅRTH  THIS IS YÖÜR CÄPTÅIN SPEÄKING  YÖÜR ØÅPTÅIN IS DEĂD«\n";
+
+            _terminal.Write(cp437);
+            _terminal.Write(plus);
         }
 
         protected override void Update(float delta)
         {
             _terminal.Update(delta);
-
             if (!_terminal.IsReadingInput)
             {
                 _terminal.Write("C:\\>");
                 _terminal.ReadLine();
+            }
+
+            for (var y = _terminal.VgaScreen.Margins.Top; y <= _terminal.VgaScreen.WindowRows; y++)
+            {
+                for (var x = _terminal.VgaScreen.Margins.Left; x <= _terminal.VgaScreen.WindowColumns; x++)
+                {
+                    _terminal.VgaScreen[x, y].Foreground = new(
+                        (byte)((2 * y + (int)PerformanceCounter.LifetimeFrames) % 256),
+                        (byte)((4 * y + (int)PerformanceCounter.LifetimeFrames) % 256),
+                        (byte)((6 * y + (int)PerformanceCounter.LifetimeFrames) % 256)
+                    );
+                }
             }
         }
 
--- Chroma.FlexTerm/TerminalFont.cs
+++ Chroma.FlexTerm/TerminalFont.cs
@@ -4,11 +4,15 @@
     {
         Acer_9x14,
         ATI_9x16,
+        AST_8x19,
+        Copam_8x8,
+        IBM_VGA_8x16,
         IGS_8x16,
         MBytePC_8x16,
+        PhoenixVGA_8x14,
         Tandy1K_II_9x14,
         ToshibaSat_8x14,
         Trident_8x16,
-        Copam_8x8
+        Verite_8x16
     }
 }
